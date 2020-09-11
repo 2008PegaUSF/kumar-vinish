1328cc10c33c010b2a42c56938b25a9d703e2f01
vinishmkumar <vinish@live.unc.edu>
2020-09-10 21:41:58 -0400

    Update UserDao.java

--- Kumar_JDBCBank/src/main/java/dao/UserDao.java
+++ Kumar_JDBCBank/src/main/java/dao/UserDao.java
@@ -8,6 +8,12 @@ import beans.User;
 
 public interface UserDao {
 	public List<User> getUserList() throws SQLException;
-	
 	public void makeNewUser(Customer newCust) throws SQLException;
+	public List<User> getUserAccounts(String fullname) throws SQLException;
+	public void deleteUserAccount(String username) throws SQLException;
+	public void transaction(int funds, String username) throws SQLException;
+	public void deleteUser(String fullname) throws SQLException;
+	public void updateUserFullName(String newname, String oldname) throws SQLException;
+	public void updateAccountUserName(String newuser, String olduser) throws SQLException;
+	public void updateAccountPassWord(String newpass, String oldpass) throws SQLException;
 }
